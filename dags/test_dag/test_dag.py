8f5a1a8d1e48448a4858ae6a37b113f6b3a2f84d
Data-drone <bpl.law@gmail.com>
2021-10-25 23:34:13 +1100

    added test dag

---
+++ dags/test_dag/test_dag.py
@@ -0,0 +1,42 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime, timedelta
+
+default_args = {
+  'owner': 'brian',
+  'depends_on_past': False,
+  'start_date': datetime(2021, 5, 25),
+  'email': [''],
+  'email_on_failure': False,
+  'email_on_retry': False,
+  'retries': 2,
+  'retry_delay': timedelta(minutes=1),
+}
+
+dag = DAG(
+    dag_id='test_dag',
+    default_args=default_args,
+    catchup=False,
+    schedule_interval=None
+)
+
+def print_start():
+    print('Start')
+    return 'Logs'
+
+def print_end():
+    print('End')
+    return 'Logs'
+
+
+start = PythonOperator(
+    task_id='test_start',
+    python_callable=print_start,
+)
+
+end = PythonOperator(
+    task_id='test_start',
+    python_callable=print_start,
+)
+
+start >> end
